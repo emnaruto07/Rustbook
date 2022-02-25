a91c2672032031e758841bf42db8929abdc1910a
emnaruto07 <shazebvps@gmail.com>
2022-02-24 20:40:53 -0800

    added cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-b3f652553177219c.d
@@ -0,0 +1,5 @@
+/home/shell/Desktop/Rustbook/hello_cargo/target/debug/deps/hello_cargo-b3f652553177219c.rmeta: src/main.rs
+
+/home/shell/Desktop/Rustbook/hello_cargo/target/debug/deps/hello_cargo-b3f652553177219c.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/shell/Desktop/Rustbook/hello_cargo/target/debug/hello_cargo: /home/shell/Desktop/Rustbook/hello_cargo/src/main.rs
