999b9a4ea79fe8de42d1f46ab13ff8012da6d473
emnaruto07 <shazebvps@gmail.com>
2022-02-26 00:59:30 -0800

    variables

---
+++ variables/target/debug/deps/variables-c9427fffd5c7c93d.d
@@ -0,0 +1,5 @@
+/home/shell/Desktop/Rustbook/variables/target/debug/deps/variables-c9427fffd5c7c93d: src/main.rs
+
+/home/shell/Desktop/Rustbook/variables/target/debug/deps/variables-c9427fffd5c7c93d.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/shell/Desktop/Rustbook/variables/target/debug/variables: /home/shell/Desktop/Rustbook/variables/src/main.rs
